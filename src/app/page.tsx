54c5270bd6cb10aff9093cce41f3d205a434a453
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2024-10-17 17:24:42 +0100

    Supabase client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
-"use client";
-export default async function Index() {
+import { supabase } from "@/utils/supabase/client";
+
+export default function Home() {
+  const createFarmer = async () => {
+    const { data, error} = await supabase
+      .from("farmers")
+      .insert({
+        name: 'Ibrahim Uzairu',
+        email: 'ibrahimuzairu28@gmail.com',
+        location: 'Katsina',
+        phone_number: '08034566343',
+        image_url: 'hdkjkdhhgahdhdjj'
+      }
+      );
+
+      if (data) {
+        console.log('Farmer created:', data);
+      }
+      if (error) {
+        console.error('Error:', error);
+      }  
+  };
+
+  createFarmer();
+
   return (
+
     <div>Logged in</div>
   );
 }
